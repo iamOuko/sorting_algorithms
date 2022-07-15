991aed535eb1c206aff5e83439dbd8dc84147d07
ouko <oukoezzy@gmail.com>
2022-07-15 11:41:50 +0300

    quick sort

--- sort.h
+++ sort.h
@@ -29,4 +29,5 @@ void counting_sort(int *array, size_t size);
 /*Selection sort*/
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 #endif
